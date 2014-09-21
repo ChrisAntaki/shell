7c36cc0ceab30954847e00cbf02105a73520f95a
Chris Antaki <ChrisAntaki@gmail.com>
2014-09-21 10:43:25 -0700

    Added Git aliases

---
+++ scripts/git.sh
@@ -0,0 +1,16 @@
+#########
+## Git ##
+#########
+
+alias add="git add"
+alias amend="git commit --amend"
+alias init="git init"
+alias k="gitk"
+alias log="git log"
+alias pull="git pull"
+alias push="git push"
+alias radd="git remote add"
+alias remove="git rm"
+alias rremove="git remote rm"
+alias save="git commit -m"
+alias st="git status"
