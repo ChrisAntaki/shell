82105b7067fc91ee65b129c8805076c355c86e56
Chris Antaki <ChrisAntaki@gmail.com>
2015-03-03 11:18:20 -0800

    Copy & paste

---
+++ scripts/copy.sh
@@ -0,0 +1,12 @@
+COPY_SH_DATA_PATH=$COPY_SH_DIR/scripts/copy.sh.txt
+
+function copy() {
+    echo $1 > $COPY_SH_DATA_PATH
+    __copy_to_clipboard
+}
+
+function __copy_to_clipboard() {
+    paste=`head -1 $COPY_SH_DATA_PATH`
+}
+
+__copy_to_clipboard
